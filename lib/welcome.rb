5424c985e081eeb695b39fdcded81751cba8bce2
Socorro Diaz <sodays1@gmail.com>
2018-06-06 05:14:55 +0000

    automatically backed up by learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -7,7 +7,10 @@ puts "Welcome to Tic Tac Toe!"
 ruby lib/welcome.#!/usr/bin/env ruby -wKU
 puts "Welcome to Tic Tac Toe!" in 'lib/welcome.rb'
 run ruby lib/welcome.#!/usr/bin/env ruby -wKU
+<<<<<<< HEAD
 puts "Welcome to Tic Tac Toe"
 "Welcome to Tic Tac Toe"
 ruby lib/welcome.#!/usr/bin/env ruby -wKU
 puts "Welcome to Tic Tac Toe!" in lib/welcome.#!/usr/bin/env ruby -wKU
+=======
+>>>>>>> f7f7b9f0fb801ea0d5c8d3b979394a29d7debd5d
