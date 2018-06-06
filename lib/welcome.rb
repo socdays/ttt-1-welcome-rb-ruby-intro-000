7aef6fd06415cf41bb9b0c443051d43721601882
Socorro Diaz <sodays1@gmail.com>
2018-06-06 01:52:28 +0000

    Done.

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,3 +1,12 @@
 # 'puts' "Welcome to Tic Tac Toe!"
 # You can see what this file does by running:
 # ruby lib/welcome.rb from your terminal.
+ruby lib/welcome.#!/usr/bin/env ruby -wKU
+puts "Welcome to Tic Tac Toe!"
+
+ruby lib/welcome.#!/usr/bin/env ruby -wKU
+puts "Welcome to Tic Tac Toe!" in 'lib/welcome.rb'
+run ruby lib/welcome.#!/usr/bin/env ruby -wKU
+puts "Welcome to Tic Tac Toe"
+"Welcome to Tic Tac Toe"
+ruby lib/welcome.#!/usr/bin/env ruby -wKU
